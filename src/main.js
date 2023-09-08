89ce028e1e19d91f3e5ffe6d373ad9c2a44240d3
Charles Yaris <charles.yaris.-nd@disney.com>
2023-09-08 16:31:40 -0500

    removed comments.

--- src/main.js
+++ src/main.js
@@ -6,9 +6,6 @@ import "../node_modules/svelte-lib/src/lib/static/styles/app.css"
 let div = document.createElement("div")
 div.classList.add("profile-photo")
 
-  // console.log(profilePhoto)
-// document.body.appendChild(profilePhoto)
-
 let script = document.currentScript
 script.parentNode.insertBefore(div, script)
 
