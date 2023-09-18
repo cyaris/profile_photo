9f4f96cdb5555994f736b05fab04715cdf7870bc
Charles Yaris <charles.yaris.-nd@disney.com>
2023-09-18 16:22:33 -0500

    including json in rollup.

--- src/lib/components/ProfilePhoto.svelte
+++ src/lib/components/ProfilePhoto.svelte
@@ -1,13 +1,38 @@
 <script>
+  import profilePhotoSrc from "../static/profile_photo.png"
+  import pixels from "../static/proile_photo_pixel_rgb_codes.json"
+  import hoverSections from "../static/proile_photo_hover_sections.json"
+
+  console.log(pixels)
   let width
   let height
+  let pixelWidth
+  let pixelHeight
+  $: {
+    if (width && height) {
+      pixelWidth = width / (Math.max(...pixels.map(v => v.x)) + 1)
+      pixelHeight = height / (Math.max(...pixels.map(v => v.y)) + 1)
+    }
+  }
 </script>
 
 <div class="flex flex-col items-center">
   <div class="flex-col w-fit h-full max-w-md" bind:clientWidth={width} bind:clientHeight={height}>
-    <img src="https://s3.us-east-2.amazonaws.com/cyaris.github.io/profile_photo/profile_photo.png" />
+    <img src={profilePhotoSrc} />
   </div>
-  <svg class="absolute" id="profile_photo" {width} {height}>
-    <rect x={0} y={0} {width} {height} fill="transparent" stroke="red" stroke-width={10} />
-  </svg>
+  {#if pixelWidth && pixelHeight}
+    <svg class="absolute" id="profile_photo" {width} {height}>
+      {#each pixels as pixel}
+        <rect
+          x={pixel.x * pixelWidth}
+          y={pixel.y * pixelHeight}
+          width={pixelWidth}
+          height={pixelHeight}
+          fill={pixel.rgb}
+          stroke="white"
+          stroke-width={0.075}
+        />
+      {/each}
+    </svg>
+  {/if}
 </div>
