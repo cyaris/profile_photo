1bac13b97edd017b5bade4ed8e7c9c53ab6f5839
Charles Yaris <charles.yaris.-nd@disney.com>
2023-09-18 19:19:02 -0500

    added transition mode to profilePhoto component. Additional modes to be added.

--- pil_pixel_photo.py
+++ pil_pixel_photo.py
@@ -3,7 +3,7 @@ import pandas as pd
 from copy import deepcopy
 
 # Open Paddington
-img = Image.open("./src/lib/static/profile_photo.png")
+img = Image.open("./src/lib/static/favicon.png")
 
 # Resize smoothly down to 48x48 pixels
 xMax = 48
@@ -18,19 +18,8 @@ for x in range(xMax):
         pixel_rgb = deepcopy(imgSmall.getpixel((int(x), int(y))))
         pixel_df.loc[row, "x"] = x
         pixel_df.loc[row, "y"] = y
+        pixel_df.loc[row, "id"] = "x" + str(x + 1) + "y" + str(y + 1)
         pixel_df.loc[row, "rgb"] = f"rgb({str(pixel_rgb[0])}, {str(pixel_rgb[1])}, {str(pixel_rgb[2])})"
         row += 1
 
-pixel_df.to_json(orient="records", path_or_buf="./src/lib/static/proile_photo_pixel_rgb_codes.json")
-
-xMax = 12
-yMax = 12
-hover_df = pd.DataFrame()
-row = 0
-for x in range(xMax):
-    for y in range(yMax):
-        hover_df.loc[row, "x"] = x
-        hover_df.loc[row, "y"] = y
-        row += 1
-
-hover_df.to_json(orient="records", path_or_buf="./src/lib/static/proile_photo_hover_sections.json")
+pixel_df.to_json(orient="records", path_or_buf="./src/lib/static/pixels.json")
--- src/lib/components/ProfilePhoto.svelte
+++ src/lib/components/ProfilePhoto.svelte
@@ -1,38 +1,125 @@
 <script>
-  import profilePhotoSrc from "../static/profile_photo.png"
-  import pixels from "../static/proile_photo_pixel_rgb_codes.json"
-  import hoverSections from "../static/proile_photo_hover_sections.json"
+  import * as d3 from "d3"
 
-  console.log(pixels)
+  import profilePhotoSrc from "../static/favicon.png"
+  import pixels from "../static/pixels.json"
+  import relativeTransitionPixels from "../static/relative_transition_pixels.json"
+
   let width
   let height
   let pixelWidth
   let pixelHeight
   $: {
     if (width && height) {
-      pixelWidth = width / (Math.max(...pixels.map(v => v.x)) + 1)
-      pixelHeight = height / (Math.max(...pixels.map(v => v.y)) + 1)
+      pixelWidth = width / Math.max(...pixels.map(v => v.x + 1))
+      pixelHeight = height / Math.max(...pixels.map(v => v.y + 1))
+
+      d3.select("#profile_photo")
+        .selectAll()
+        .data(pixels)
+        .enter()
+        .append("rect")
+        .attr("id", d => d.id)
+        .attr("x", d => d.x * pixelWidth)
+        .attr("y", d => d.y * pixelHeight)
+        .attr("width", pixelWidth)
+        .attr("height", pixelHeight)
+        .style("stroke", "white")
+        .style("stroke-width", 0.075)
+        .style("fill", d => d.rgb)
+        .on("mouseover", pixelMouseOver)
+        .on("mouseleave", pixelMouseLeave)
     }
+  }
+
+  let pixelTransitionDelay = 100
+  let pixelTransitionDuration = 750
+  let pixelTransitionLength = pixelTransitionDelay * 2 + pixelTransitionDuration * 2
+
+  let transitioningPixels = []
+  let pixelMouseOver = function (d) {
+    let transitionSelect = getTransitionSelect(d3.select(this).attr("id"))
+
+    d3.select("#profile_photo")
+      .selectAll(transitionSelect)
+      .style("stroke-width", 0.3)
+      .transition()
+      .delay(pixelTransitionDelay)
+      .duration(pixelTransitionDuration)
+      .attr("x", d => d.x * pixelWidth + 1.5)
+      .attr("y", d => d.y * pixelHeight - 1)
+      .attr("width", pixelWidth / 1.5)
+      .attr("height", pixelHeight / 1.5)
+      .attr(
+        "transform",
+        d =>
+          "rotate(45," +
+          (d.x * pixelWidth + (pixelWidth - pixelWidth / 1.5) / 2) +
+          "," +
+          (d.y * pixelHeight + (pixelHeight - pixelHeight / 1.5)) +
+          ")"
+      )
+      .transition()
+      .delay(pixelTransitionDelay)
+      .duration(pixelTransitionDuration)
+      .style("opacity", 0)
+      .on("end", () => {
+        transitioningPixels = transitioningPixels.filter(v => v !== d.id)
+        // // pixels will be recreated when the slider is used
+        // // removing those that have been activated for reveal modes.
+        // if (transitionSlider.value() != 2) {
+        //   d3.select(this).remove()
+        // }
+      })
+  }
+
+  let pixelMouseLeave = function (d) {
+    // mouseleave function is only needed for transition mode because otherwise the pixel will be removed.
+    // if (transitionSlider.value() == 2) {
+
+    let transitionSelect = getTransitionSelect(d3.select(this).attr("id"))
+    d3.select("#profile_photo")
+      .selectAll(transitionSelect)
+      .transition()
+      .delay(pixelTransitionLength + 500)
+      .duration(pixelTransitionDuration)
+      .attr("x", d => d.x * pixelWidth)
+      .attr("y", d => d.y * pixelHeight)
+      .attr("width", pixelWidth)
+      .attr("height", pixelHeight)
+      .attr(
+        "transform",
+        d =>
+          "rotate(0," +
+          (d.x * pixelWidth + (pixelWidth - pixelWidth / 1.5) / 2) +
+          "," +
+          (d.y * pixelHeight + (pixelHeight - pixelHeight / 1.5)) +
+          ")"
+      )
+      .style("opacity", 1)
+      .transition()
+      .delay(pixelTransitionDelay)
+      .duration(pixelTransitionDuration)
+      .style("stroke-width", 0.075)
+    // }
+  }
+
+  function getTransitionSelect(id) {
+    let x = parseInt(id.split("y")[0].substring(1))
+    let y = parseInt(id.split("y")[1])
+
+    return relativeTransitionPixels
+      .map(v => "#x" + String(v.x + x) + "y" + String(v.y + y))
+      .filter(v => !transitioningPixels.includes(v) && !d3.select(v).empty())
+      .join(", ")
   }
 </script>
 
 <div class="flex flex-col items-center">
-  <div class="flex-col w-fit h-full max-w-md" bind:clientWidth={width} bind:clientHeight={height}>
+  <div class="flex-col w-fit max-w-md" bind:clientWidth={width} bind:clientHeight={height}>
     <img src={profilePhotoSrc} />
   </div>
   {#if pixelWidth && pixelHeight}
-    <svg class="absolute" id="profile_photo" {width} {height}>
-      {#each pixels as pixel}
-        <rect
-          x={pixel.x * pixelWidth}
-          y={pixel.y * pixelHeight}
-          width={pixelWidth}
-          height={pixelHeight}
-          fill={pixel.rgb}
-          stroke="white"
-          stroke-width={0.075}
-        />
-      {/each}
-    </svg>
+    <svg class="absolute" id="profile_photo" {width} {height} />
   {/if}
 </div>
