f43110aee42259257944ca5b2257d8d1ef5d95a3
Charlie Yaris <charlieyaris@Charlies-MacBook-Pro.local>
2026-06-30 23:10:35 -0500

    Update profile photo loading and gauge colors

--- frontend/src/lib/components/ProfilePhoto.svelte
+++ frontend/src/lib/components/ProfilePhoto.svelte
@@ -1,13 +1,12 @@
 <script>
   import "d3-transition"
 
-  import { scaleSequential, scaleThreshold } from "d3-scale"
-  import { interpolateGreens } from "d3-scale-chromatic"
+  import { scaleLinear } from "d3-scale"
   import { select } from "d3-selection"
   import { interval } from "d3-timer"
   import { FireworkShow } from "fireworks/components"
   import { onDestroy } from "svelte"
-  import { GaugeChart, Slider } from "svelte-lib/components"
+  import { GaugeChart, Loading, Slider } from "svelte-lib/components"
 
   import profilePhotoSrc from "../static/favicon.png"
   import pixels from "../static/pixels.json"
@@ -47,14 +46,9 @@
   let sliderValue = 0
   let laserEyesTimer
 
-  const gaugeColorScale = scaleSequential(interpolateGreens).domain([0, 1]).clamp(true)
+  const gaugeColorScale = scaleLinear().domain([0, 1]).range(["#F7FCF5", "#006D2C"]).clamp(true)
 
   const fireworkRevealTrigger = 0.9
-
-  const metricColorScale = scaleThreshold()
-    .domain([fireworkRevealTrigger])
-    // First color originated from Tailwind "red-500"; second is interpolateGreens(1).
-    .range(["#ef4444", "#00441b"])
 
   function getTransitionIds(id) {
     let x = parseInt(id.split("y")[0].substring(1))
@@ -187,7 +181,7 @@
 
   let items = [
     { value: "reveal", label: "Reveal" },
-    { value: "reveal_my_laser_vision", label: "Reveal My Laser Vision" },
+    { value: "reveal_laser_vision", label: "Reveal My Laser Vision" },
     { value: "transition", label: "Transition" },
   ]
 
@@ -222,37 +216,40 @@
     <g bind:this={laserEyeCanvas}></g>
     <g bind:this={pixelCanvas}></g>
   </svg>
-  <div class="mt-4 flex flex-col items-center">
-    <Slider
-      wrapperClasses="w-80 text-sm"
-      value={sliderValue}
-      {items}
-      min={0}
-      max={2}
-      hoverable={false}
-      springValues={{ stiffness: 1, damping: 1 }}
-      on:valueChange={handleSliderValueChange}
-    />
-  </div>
-  <div class="mt-4 flex flex-col items-center">
-    <div class="text-xl">Hover on my face!</div>
-    {#if sliderValue !== 2}
-      <div class="w-52">
-        <GaugeChart
-          value={(revealed.length / pixels.length) * 100}
-          addPercentSign={true}
-          title="Pixels Revealed"
-          titlePosition="bottom"
-          decimalPlaces={1}
-          titleClasses="text-xl"
-          textClasses="font-medium"
-          definition="Can you reveal 90%?"
-          {gaugeColorScale}
-          {metricColorScale}
-        />
-      </div>
-    {/if}
-  </div>
+  {#if laserEyeCanvas && pixelCanvas}
+    <div class="mt-2 flex flex-col items-center">
+      <Slider
+        wrapperClasses="w-80 text-sm"
+        value={sliderValue}
+        {items}
+        min={0}
+        max={2}
+        hoverable={false}
+        springValues={{ stiffness: 1, damping: 1 }}
+        on:valueChange={handleSliderValueChange}
+      />
+    </div>
+    <div class="mt-4 flex flex-col items-center">
+      <div class="text-xl">Hover on my face!</div>
+      {#if sliderValue !== 2}
+        <div class="w-52">
+          <GaugeChart
+            value={(revealed.length / pixels.length) * 100}
+            addPercentSign={true}
+            title="Pixels Revealed"
+            titlePosition="bottom"
+            decimalPlaces={1}
+            titleClasses="text-xl"
+            textClasses="font-normal"
+            definition="Can you reveal 90%?"
+            {gaugeColorScale}
+          />
+        </div>
+      {/if}
+    </div>
+  {:else}
+    <Loading classes="mt-6 h-12 w-12" image="circle" />
+  {/if}
 </div>
 {#if sliderValue !== 2 && revealed.length / pixels.length >= fireworkRevealTrigger}
   <div class="non-reactive fixed left-0 top-0">
